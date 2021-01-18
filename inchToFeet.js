90557a1bd22f3922a7a574d117c83434084b36e0
aar-atiq <aar.msdeveloper01@gmail.com>
2021-01-18 23:45:30 +0600

    inchToFeet converter

---
+++ inchToFeet.js
@@ -0,0 +1,8 @@
+function inchTofeet(inchNumber){
+    var feet = inchNumber/12;
+    return feet;
+}
+var output = inchTofeet(288);
+console.log(output);
+var output1 = inchTofeet(156);
+console.log(output1);
